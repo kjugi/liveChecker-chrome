f2ef2a63abfea94957a9725275d8cbc9c4691461
kjugi <filipkud31@gmail.com>
2017-01-16 22:13:10 +0100

    core files of extension
    dream version of plugin - It will be developed in the future

---
+++ background.js
@@ -0,0 +1,162 @@
+/* function which will get stream status from TWITCH API
+
+YES, TWITCH API
+Kappa
+
+#PAPUT
+*/
+
+//variable to playing PAGO audio - łapki w góre
+var oneMoreTimeHomie = true, audio, volumeLevelDecimal;
+
+function getTwitchStreamStatus(streamOn, streamOff, errorCallback){
+	//one streamer extension PAGO3
+	//demo mode: on
+	//https://api.twitch.tv/kraken/streams/40378922?client_id=dlw882cebptkltr33a4s0ejzongxfg
+	var streamerID = "40378922";
+	var client_id = "dlw882cebptkltr33a4s0ejzongxfg";
+	var url = "https://api.twitch.tv/kraken/streams/";
+  	var data = null;
+
+	var xhr = new XMLHttpRequest();
+
+	xhr.open("GET", url+streamerID);
+	xhr.setRequestHeader("accept", "application/vnd.twitchtv.v5+json");
+	xhr.setRequestHeader("client-id", client_id);
+	xhr.setRequestHeader("cache-control", "no-cache");
+	xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+
+  	xhr.onload = function() {
+	    var response = JSON.parse(xhr.responseText);
+
+	    if (!response) {
+	      showInfo("error");
+	      errorCallback('No response from TWITCH API!');
+	      return;
+	    }
+
+	    if(response.stream != null){
+		    var streamTitle = response.stream.channel.status;
+		    var streamGame = response.stream.game;
+		    var streamLiveViewers = response.stream.viewers;
+		    var streamLiveDate = response.stream.created_at;
+		    var streamPreviewMedium = response.stream.preview.medium;
+
+		    console.assert(
+		        typeof response == 'object', 'Unexpected response from the TWITCH API!');
+
+		    streamOn(streamTitle, streamGame, streamLiveViewers, streamLiveDate, streamPreviewMedium);
+		}
+		else{
+			var streamNull = "Brak streama!";
+
+			console.assert(
+				typeof response == 'object', 'Unexpected response from the TWITCH API!');
+
+			streamOff(streamNull);
+		}
+	};
+	xhr.onerror = function() {
+		showInfo("error");
+	    errorCallback('Network error.');
+	};
+	xhr.send(data);
+}
+
+//checking on welcome if stream is live function - START
+function checkLiveStream(){
+	getTwitchStreamStatus(function(streamTitle, streamGame, streamLiveViewers, streamLiveDate, streamPreviewMedium) {
+		chrome.browserAction.setIcon({path: "icons/icon_1.png"});
+		chrome.browserAction.setBadgeBackgroundColor({color:[208, 0, 24, 255]});
+ 		chrome.browserAction.setBadgeText({text:"ON"});
+
+ 		//start audio play - only when stream start
+ 		playMusic(volumeLevelDecimal);
+	},
+	function(streamOff){
+		chrome.browserAction.setIcon({path: "icons/icon_default.png"});
+		chrome.browserAction.setBadgeText({text: ''});
+
+		//to play again when stream went on
+		oneMoreTimeHomie = true;
+	},
+	function(errorMessage) {
+		showInfo("error");
+	  	console.log('Cannot display information. ' + errorMessage);
+	});
+}
+checkLiveStream();
+//checking on welcome if stream is live function - END
+
+//getting localStorage - START
+function getOptions(){
+	chrome.storage.sync.get([
+		"options"
+	],function(items){
+		if(typeof items.options == "undefined"){
+			setDefaultFirstOptions();
+		}
+		else{
+			var itemsArray = JSON.parse(items.options);
+			var miliseconds, interval, volumeLevelPercent;
+
+			if(itemsArray.length == 4){
+				if(itemsArray[0] === false && itemsArray[1] === false){
+					miliseconds = 0;
+					clearInterval(interval);
+				}
+				else{
+					clearInterval(interval);
+					miliseconds = itemsArray[1] * 60000;
+					interval = setInterval(checkLiveStream, miliseconds);
+				}
+
+				if(itemsArray[2] != false && itemsArray[3] != false){
+					volumeLevelPercent = itemsArray[3];
+					volumeLevelDecimal = itemsArray[3] * 0.01;
+				}
+			}
+			else{
+				clearInterval(interval);
+				miliseconds = 3600000;
+				interval = setInterval(checkLiveStream,miliseconds);
+
+				volumeLevelDecimal = 0.5;
+			}
+		}
+	});
+}
+getOptions();
+//getting localStorage - END
+
+//checking localStorage on change [new click on save button in options]
+chrome.storage.onChanged.addListener(function(){
+	getOptions();
+});
+
+//setting first default options - ONLY ON FIRST RUN
+function setDefaultFirstOptions(){
+	var arrayValues = [true,'50'];
+	var jsonArray = JSON.stringify(arrayValues);
+
+	chrome.storage.sync.set({
+		"options":jsonArray
+	});
+
+	getOptions();
+}
+
+//start audio play - only when stream starts
+function playMusic(volumeLevel){
+	if(typeof volumeLevel != "undefined"){
+		if(oneMoreTimeHomie == true){
+			audio = new Audio('audio/pago_lapki_w_gore.mp3');
+			audio.volume = volumeLevel;
+			audio.play();
+		}
+	}
+
+	//to stop playing till the stream will ends
+	oneMoreTimeHomie = false;
+}
+
---
+++ popup.js
@@ -0,0 +1,360 @@
+/* function which will get stream status from TWITCH API
+
+YES, TWITCH API
+Kappa
+
+#PAPUT
+*/
+
+// function which getting information from TWTICH API
+function getTwitchStreamStatus(streamOn, streamOff, errorCallback){
+	//one streamer extension PAGO3
+	//demo mode: on
+	//https://api.twitch.tv/kraken/streams/40378922?client_id=dlw882cebptkltr33a4s0ejzongxfg
+
+	var streamerID = "40378922";
+	var client_id = "dlw882cebptkltr33a4s0ejzongxfg";
+	var url = "https://api.twitch.tv/kraken/streams/";
+  	var data = null;
+
+	var xhr = new XMLHttpRequest();
+
+	xhr.open("GET", url+streamerID);
+	xhr.setRequestHeader("accept", "application/vnd.twitchtv.v5+json");
+	xhr.setRequestHeader("client-id", client_id);
+	xhr.setRequestHeader("cache-control", "no-cache");
+	xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+
+  	xhr.onload = function() {
+	    var response = JSON.parse(xhr.responseText);
+	    //error control - renderStatus("response: "+response);
+
+	    if (!response) {
+	      showInfo("error");
+	      errorCallback('No response from TWITCH API!');
+	      return;
+	    }
+
+	    if(response.stream != null){
+		    var streamTitle = response.stream.channel.status;
+		    var streamGame = response.stream.game;
+		    var streamLiveViewers = response.stream.viewers;
+		    var streamLiveDate = response.stream.created_at;
+		    var streamPreviewMedium = response.stream.preview.medium;
+
+		    console.assert(
+		        typeof response == 'object', 'Unexpected response from the TWITCH API!');
+		    streamOn(streamTitle, streamGame, streamLiveViewers, streamLiveDate, streamPreviewMedium);
+		}
+		else{
+			var streamNull = "Brak streama!";
+
+			console.assert(
+				typeof response == 'object', 'Unexpected response from the TWITCH API!');
+			streamOff(streamNull);
+		}
+	};
+	xhr.onerror = function() {
+		showInfo("error");
+	    errorCallback('Network error.');
+	};
+	xhr.send(data);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	
+	//checking stream status in twitch API - big function - START
+	getTwitchStreamStatus(function(streamTitle, streamGame, streamLiveViewers, streamLiveDate, streamPreviewMedium) {
+
+	  var status = document.getElementById('status');
+	  var dateNowTimestamp = new Date();
+	  var streamStartDateTimestamp = new Date(streamLiveDate);
+
+	  var streamDuring = dateNowTimestamp - streamStartDateTimestamp;
+
+	  //TO DO: function do this - form multiple uses
+	  if(streamDuring > 0){
+	  	var seconds = (streamDuring/1000)%60;
+	  	var minutes = ((streamDuring-seconds)/1000)/60;
+
+	  	var outputTime = "";
+
+	  	var intMinutes = parseInt(minutes);
+	  	var intSeconds = parseInt(seconds);
+
+	  	if(minutes == 1){
+	  		outputTime = intMinutes+"minuty ";
+	  		if(seconds > 1){
+	  			outputTime += intSeconds+"sekund";
+	  		}
+	  		else if(seconds == 1){
+	  			outputTime += intSeconds+"sekundy";
+	  		}
+	  	}
+	  	else if(minutes == 0){
+	  		outputTime = intSeconds+"sekund";
+	  	}
+	  	else{
+	  		outputTime = intMinutes+"minut ";
+	  		if(seconds > 1){
+	  			outputTime += intSeconds+"sekund";
+	  		}
+	  		else if(seconds == 1){
+	  			outputTime += intSeconds+"sekundy";
+	  		}
+	  	}
+	  }
+	  else{
+	  	showInfo("error");
+	  	errorCallback('Time error 1');
+	  }
+
+	  	chrome.browserAction.setIcon({path: "icons/icon_1.png"});
+	  	chrome.browserAction.setBadgeBackgroundColor({color:[208, 0, 24, 255]});
+ 		chrome.browserAction.setBadgeText({text:"ON"});
+
+		status.innerHTML = "<p class='handler__text'>Tytuł streama: "+streamTitle+"</p><p class='handler__text'>Gra: "+streamGame+"</p><p class='handler__text'> Bynie online: <span class='handler__text-live'>"+streamLiveViewers+"</span></p><p class='handler__text'>Live trwa od: "+outputTime+"</p><a href='https://twitch.tv/pago3/' target='_blank' class='handler__text__stream text-xs-center'><img src='"+streamPreviewMedium+"'/></a><a href='https://twitch.tv/pago3' target='_blank' class='handler__text-button-watch'>Oglądaj bynia!</a>";
+	},
+	function(streamOff){
+		var status = document.getElementById('status');
+
+		chrome.browserAction.setIcon({path: "icons/icon_default.png"});
+		chrome.browserAction.setBadgeText({text: ''});
+
+		status.innerHTML = "<p class='handler__text text-xs-center'>"+streamOff+"</p><img src='icons/dead_glitch.png' class='image-center'/><p class='handler__text'>Sprawdź co na naszej grupie: <a href='https://www.facebook.com/groups/1721694058053294/' target='_blank'>Bynie Pągowskiego</a></p>";
+	},
+	function(errorMessage) {
+		showInfo("error");
+		renderStatus('Cannot display information. ' + errorMessage);
+	});
+	//checking stream status in twitch API - big function - END
+
+	//showing/hiding options
+	document.getElementById("gear").addEventListener("click", function(){
+		var divOptions = document.getElementById("options");
+		var classList = divOptions.className.split(/\s+/);
+
+		if(classList[1] == "none"){
+			divOptions.className = divOptions.className.replace(/\bnone\b/,'');
+		}
+		else{
+			divOptions.className += "none";
+		}
+	});
+
+	//range - options
+	document.getElementById("range").addEventListener("mousemove", function(){
+		rangeOptions("range","rangeValue","min");
+	});
+	document.getElementById("volume").addEventListener("mousemove", function(){
+		rangeOptions("volume","volumeValue","%");
+	});
+
+	//setting timeout time of call twitch api - options
+	document.getElementById("timeout").addEventListener("click", function(){
+		var timeout = document.getElementById("timeout");
+		var range = document.getElementById("range");
+		var rangeValue = document.getElementById("rangeValue");
+
+		var valueCheckbox = timeout.checked;
+
+		if(valueCheckbox == true){
+			timeout.checked = true;
+			range.disabled = false;
+			range.value = 60;
+			rangeValue.innerHTML = "60min";
+		}
+		else{
+			range.disabled = true;
+			rangeValue.innerHTML = "Opcja wyłaczona";
+		}
+	});
+
+	//setting volume height of audio alert - options
+	document.getElementById("voiceAlert").addEventListener("click",function(){
+		var voiceAlert = document.getElementById("voiceAlert");
+		var volume = document.getElementById("volume");
+		var volumeValue = document.getElementById("volumeValue");
+
+		var valueCheckbox = voiceAlert.checked;
+
+		if(valueCheckbox == true){
+			voiceAlert.checked = true;
+			volume.disabled = false;
+			volume.value = 50;
+			volumeValue.innerHTML = "50%";
+		}
+		else{
+			volume.disabled = true;
+			volumeValue.innerHTML = "Opcja wyłaczona";
+		}
+	});
+
+	//saving options into localStorage - START
+	var arrayValues = new Array();
+	document.getElementById("saveFunctions").addEventListener("click", function(){
+		arrayValues = [];
+		var timeoutCheckboxValue = document.getElementById("timeout").checked;
+		var voiceAlertCheckboxValue = document.getElementById("voiceAlert").checked;
+
+			arrayValues.push(timeoutCheckboxValue);
+		pushToLocalStorage(timeoutCheckboxValue,"range");
+
+			arrayValues.push(voiceAlertCheckboxValue);
+		pushToLocalStorage(voiceAlertCheckboxValue,"volume");
+
+		var jsonArray = JSON.stringify(arrayValues);
+
+		chrome.storage.sync.set({
+			"options":jsonArray
+		},function(){
+			showInfo("success");
+		});
+	});
+
+	//getting options from localStorage - START
+	function getOptions(){
+		chrome.storage.sync.get([
+			"options"
+		],function(items){
+			var itemsArray = JSON.parse(items.options);
+
+			//DOM elements - START
+			var timeout = document.getElementById("timeout");
+			var range = document.getElementById("range");
+			var rangeValue = document.getElementById("rangeValue");
+
+			var voiceAlert = document.getElementById("voiceAlert");
+			var volume = document.getElementById("volume");
+			var volumeValue = document.getElementById("volumeValue");
+			//DOM elements - END
+
+			if(itemsArray.length == 4){
+				if(itemsArray[0] === false && itemsArray[1] === false){
+					timeout.checked = itemsArray[0];
+					range.disabled = true;
+					rangeValue.innerHTML = "Opcja wyłaczona";
+				}
+				else{
+					timeout.checked = itemsArray[0];
+					range.disabled = false;
+					range.value = itemsArray[1];
+					rangeValue.innerHTML = itemsArray[1]+" minut";
+				}
+
+				if(itemsArray[2] === false && itemsArray[3] === false){
+					voiceAlert.checked = itemsArray[2];
+					volume.disabled = true;
+					volumeValue.innerHTML = "Opcja wyłaczona";
+				}
+				else{
+					voiceAlert.checked = itemsArray[2];
+					volume.disabled = false;
+					volume.value = itemsArray[3];
+					volumeValue.innerHTML = itemsArray[3]+"%";
+				}
+			}
+			else{
+				timeout.checked = true;
+				range.disabled = false;
+				range.value = 60;
+				rangeValue.innerHTML = "60 minut";
+
+				voiceAlert.checked = true;
+				volume.disabled = false;
+				volume.value = 50;
+				volumeValue.innerHTML = "50%";
+			}
+		});
+	}
+	getOptions();
+	//end getting options from localStorage - END
+	//end saving options into localStorage - END
+
+	//range - options - function - START
+	function rangeOptions(idRange, idReturnedValue, unit){
+		var value = document.getElementById(idRange).value;
+		document.getElementById(idReturnedValue).innerHTML = value+unit;
+	}
+	//range - options - function - END
+
+	//values - push to array - function - START
+	function pushToLocalStorage(booleanVar, idRange){
+		if(booleanVar === true){
+			var rangeValue = document.getElementById(idRange).value;
+
+			arrayValues.push(rangeValue);
+		}
+		else{
+			arrayValues.push(false);
+		}
+	}
+	//values - push to array - function - END
+
+	//showing success/error popup - START
+	function showInfo(whichInfo){
+		if(whichInfo == "success"){
+			var popup = document.getElementById("success");
+		}
+		else if(whichInfo == "error"){
+			var popup = document.getElementById("error");
+		}
+
+		var classList = popup.className.split(/\s+/);
+
+		if(classList[1] == "hidden-popup"){
+			popup.className = popup.className.replace(/\bhidden-popup\b/,'');
+		}
+
+		setTimeout(function(){
+			hideInfo(whichInfo);
+		},2000);
+	}
+
+	function hideInfo(whichInfoToHide) {
+		if(whichInfoToHide == "success"){
+			var popup = document.getElementById("success");
+		}
+		else if(whichInfoToHide == "error"){
+			var popup = document.getElementById("error");
+		}
+
+		var classList = popup.className.split(/\s+/);
+
+		if(classList[1] != "hidden-popup"){
+			popup.className += "hidden-popup";
+		}
+	}
+	//showing success/error popup - END
+
+	//hiding success/error popup onClick event - START
+	document.getElementById('success').addEventListener('click',function(){
+		hideInfo('success');
+	});
+
+	document.getElementById('error').addEventListener('click',function(){
+		hideInfo('error');
+	});
+	//hiding success/error popup onClick event - END
+
+	//pause audio play when user clicked on icon - by getting values from background.js
+	var backgroundPage = chrome.extension.getBackgroundPage();
+	if(typeof backgroundPage.audio != "undefined"){
+ 		backgroundPage.audio.pause();
+ 	}
+});
+
+/****
+
+TO DO:
+1) nottifications in toolbar chrome - need permission/knowledge
+2) ADD option with live iframe instead of img from stream + options with autoplay and choose between img/iframe
+4) ADD bug report to my mail/ticket on github
+5) review all errors
+6) add youtube the newest movie and show it with link && title instead fb group (refreshing more times)
+.
+.
+.
+x) minute word rendering function - not really needed
+
+****/
